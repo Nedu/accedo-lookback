7215cf5623fe053880a75c07e1ace39d4f128b3c
Nedu Robert <nedu.robert@accedo.tv>
2023-09-28 13:33:45 -0400

    feat: home dashboard page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components';
 
+import AppLogo from '../../images/logo.svg';
+
+const Logo = styled.img`
+  width: 350px;
+`;
 const HomeContainer = styled.div`
   min-height: 100vh;
+`;
+
+const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  min-height: 80vh;
 `;
 
 const HomeTitle = styled.h1`
@@ -14,21 +24,47 @@ const HomeTitle = styled.h1`
   font-family: Orbitron;
   font-size: 70px;
   font-style: italic;
-  font-variant: small-caps;
   font-weight: 700;
   color: #00b9bc;
 `;
 
+const SubTitle = styled.h5`
+  margin: 0;
+  font-family: Orbitron;
+  font-size: 70px;
+  font-style: italic;
+  font-weight: 700;
+  color: #00b9bc;
+`;
+
+const ActionButton = styled.button`
+  font-family: Orbitron;
+  font-size: 70px;
+  font-style: italic;
+  font-variant: small-caps;
+  font-weight: 700;
+  color: #fff;
+  background: #00b9bc;
+  border: 1px solid #00b9bc;
+  border-radius: 50px;
+  padding: 30px;
+  margin-top: 24px;
+  cursor: pointer;
+`;
+
 
 const Home = () => {
+  const navigate = useNavigate();
+
     return (
         <React.Fragment>
           <HomeContainer>
-              <HomeTitle>Accedo Look-back</HomeTitle>
-              <form>
-                <input type={"file"} accept={".csv"} />
-                <button>IMPORT CSV</button>
-              </form>
+              <Logo src={AppLogo} alt="App Logo" />
+              <Container>
+                <HomeTitle>Accedo Look-back</HomeTitle>
+                <SubTitle>An insight on all the data collected by OTT</SubTitle>
+                <ActionButton onClick={() => navigate('/uploadPage')}>Generate Your Data</ActionButton>
+              </Container>
           </HomeContainer>
         </React.Fragment>
     );
